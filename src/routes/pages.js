489ef9e4340414c4a4cca27ee0a4b7fbb5843648
doyeon <duseh729@naver.com>
2025-09-21 06:33:02 +0900

    refactor: delete pvp nav

--- src/routes/pages.js
+++ src/routes/pages.js
@@ -8,7 +8,6 @@ export const pages = {
   home: { labelKey: "nav.home", component: Home, showInNav: true },
   adventure: { labelKey: "nav.adventure", component: Adventure, showInNav: true },
   fusion: { labelKey: "nav.fusion", component: Fusion, showInNav: true },
-  pvp: { labelKey: "nav.pvp", component: Pvp, showInNav: true },
   inventory: { labelKey: "nav.inventory", component: Inventory, showInNav: true },
 };
 
